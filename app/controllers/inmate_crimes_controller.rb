6430b7c3b9ea74046a604827e6cbe8ffb6abc65d
Douglas Berkley <douglasmberkley@gmail.com>
2019-05-23 18:59:25 +0900

    fixed the iteration of the inmate params

--- app/controllers/inmate_crimes_controller.rb
+++ app/controllers/inmate_crimes_controller.rb
@@ -6,11 +6,13 @@ class InmateCrimesController < ApplicationController
 
   def create
     @inmate = Inmate.find(params[:inmate_id])
-    @inmate_crime = nil
-    Crime.where(id: params[:inmate_crime][:crime_id]).each do |crime|
-      @inmate_crime = InmateCrime.new(crime: crime)
-      @inmate_crime.inmate = @inmate
-      @inmate_crime.save
+    @inmate_crime = InmateCrime.new
+    if params[:inmate_crime]
+      Crime.where(id: params[:inmate_crime][:crime_id]).each do |crime|
+        @inmate_crime = InmateCrime.new(crime: crime)
+        @inmate_crime.inmate = @inmate
+        @inmate_crime.save
+      end
     end
     if @inmate_crime.valid?
       redirect_to prison_path(@inmate.prison)
